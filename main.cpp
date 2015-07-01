b25c16c352afb60b6b2a239a16747beed7690424
Seth P <csparker247@gmail.com>
2015-07-01 18:15:27 -0400

    (xyT) Full frame reslicing, but use a lot of swap space. Use at your own risk. Needs to cleanup tmp files.

--- main.cpp
+++ main.cpp
@@ -1,53 +1,90 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+std::vector< std::vector<cv::Mat> > images;
+int max_slices_in_memory = 1000;
+
+void emptyImages(int suffix, int frameHeight, std::string outputDir) {
+  std::cerr << std::endl;
+  for ( int image = 0 ; image < images.size() ; ++image ) {
+    cv::Mat output(frameHeight, images[image].size(), CV_8UC3);
+    for ( int col_counter = 0; col_counter < images[image].size(); ++col_counter) {
+      std::cerr << "Stitching space to time: [" << image << "] " << col_counter + 1 << "/" << images[image].size() << "\r" << std::flush;
+      images[image][col_counter].copyTo(output.col(col_counter));
+    }
+
+    std::string outputName = outputDir + std::to_string(image) + "_" + std::to_string(suffix) + ".png";
+    cv::imwrite(outputName, output);
+
+    images[image].clear();
+  }
+  std::cerr << std::endl;
+}
+
 int main( int argc, char** argv ) {
 
   cv::VideoCapture Video( argv[1] ); // open the default camera
   if(!Video.isOpened())  // check if we succeeded
     return -1;
 
+  std::string outputDir = "";
+    outputDir = argv[2];
+
+  std::string outputFinal = "";
+    outputFinal = argv[3];
+
   double frameWidth = Video.get( CV_CAP_PROP_FRAME_WIDTH);
   double frameHeight = Video.get( CV_CAP_PROP_FRAME_HEIGHT );
   double frameTime = ceil(Video.get( CV_CAP_PROP_FRAME_COUNT )); // round up
   bool needsColorCvt = Video.get(CV_CAP_PROP_CONVERT_RGB);
 
-  std::vector<cv::Mat> columns;
+  for ( int i = 0; i < frameWidth; ++i ) {
+    std::vector<cv::Mat> columns;
+    images.push_back(columns);
+  }
 
   cv::Mat frame; // The current decoded frame
-  double current_output_column = 0; // The output column index of the current frame
+  double current_frame = 0; // The output column index of the current frame
 
   while ( ( Video.read(frame) ) != false ) {
     if ( frame.empty() ) continue;
-    std::cerr << "Unravelling the fabric of space-time for frame: " << current_output_column + 1 << "/" << frameTime;
-    if ( current_output_column > frameTime )
+    std::cerr << "Unravelling the fabric of space-time: " << current_frame + 1 << "/" << frameTime;
+    if ( current_frame > frameTime )
       std::cerr << "\?\?\?!?";
     std::cerr << "\r" << std::flush;
 
-    // For each column in the frame, copy its corresponding mat in outputImages
-    int input_col_index = frameWidth/2;
-    cv::Mat input_col_mat;
-    frame.col(input_col_index).copyTo(input_col_mat);
-    columns.push_back(input_col_mat);
-  
-    ++current_output_column; // Next column
+    // For each column in the frame, copy its corresponding col into the vector
+    for ( int input_col_index = 0; input_col_index < frameWidth; ++input_col_index ) {
+      cv::Mat input_col_mat;
+      frame.col(input_col_index).copyTo(input_col_mat);
+      images[input_col_index].push_back(input_col_mat);
+    }
+
+    if ( ((int) current_frame + 1) % max_slices_in_memory == 0 ) {
+      int suffix = (int) current_frame / max_slices_in_memory;
+      emptyImages( suffix, frameHeight, outputDir );
+    }
+
+    ++current_frame; // Next column
+  }
+
+  // Write out any lingering cols
+  int suffix = (int) current_frame / max_slices_in_memory;
+  emptyImages( suffix, frameHeight, outputDir );
+
+  // Restitch images
+  for ( int input_col_index = 0; input_col_index < frameWidth; ++input_col_index ) {
+    cv::Mat finalImage( frameHeight, frameTime, CV_8UC3 );
+    for ( int partial_img = 0; partial_img <= suffix; ++partial_img ) {
+      std::cerr << "Dealing with human limitations: [" << input_col_index << "] " << partial_img << "/" << suffix << std::flush;
+      std::string partialImgPath = outputDir + std::to_string(input_col_index) + "_" + std::to_string(partial_img) + ".png";
+      cv::Mat temp = cv::imread( partialImgPath );
+      temp.copyTo( finalImage(cv::Rect( partial_img * max_slices_in_memory, 0, temp.cols, temp.rows)) );
+    }
+    std::string outputName = outputFinal + std::to_string(input_col_index) + ".png";
+    cv::imwrite(outputName, finalImage);
   }
   std::cerr << std::endl;
-
-  cv::Mat output(frameHeight, columns.size(), CV_8UC3);
-  for ( int col_counter = 0; col_counter < columns.size(); ++col_counter) {
-    std::cerr << "Restitching reality: " << col_counter + 1 << "/" << columns.size() << "\r" << std::flush;
-    columns[col_counter].copyTo(output.col(col_counter));
-  }
-  std::cerr << std::endl;
-
-  std::string outputDir = "";
-  if ( argc > 2 )
-    outputDir = argv[2];
-
-  std::string outputName = outputDir + "output.png";
-  cv::imwrite(outputName, output);
-
 
   return 0;
 }
